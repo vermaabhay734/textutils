eb9bbeb781b75b489aa9d8f61e6f8cb3204d4e27
abhay734 <vermaabhay734@gmail.com>
2024-01-15 18:21:10 +0530

    Added UpperCase feature using State & Event

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,7 +1,17 @@
 import React, {useState} from "react";
 
-
 export default function TextForm(props) {
+
+  const handleUpClick = ()=>{
+    // console.log("Uppercase was clicked");
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+
+  const handleOnChange = (event)=>{
+    // console.log("On-change");
+    setText(event.target.value)
+  }
 
   // Hook 
   const [text, setText] = useState('Enter text here');
@@ -11,9 +21,9 @@ export default function TextForm(props) {
     <div>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="7"></textarea>
+        <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="7"></textarea>
       </div>
-      <button className="btn btn-primary">Convert to Uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   );
 }
