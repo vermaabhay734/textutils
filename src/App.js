1b94caa55ea8a66510f9d42683e4236369cbf0b6
abhay734 <vermaabhay734@gmail.com>
2024-01-17 04:04:24 +0530

    Adding Enable Dark Mode button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
       <Navbar title = "TextUtils"/>
       <div className="container my-3">
-        <TextForm heading = "Enter the text to Analyze below:" />
+        {/* <TextForm heading = "Enter the text to Analyze below:" /> */}
+        <About/>
       </div>
     </>
   );
